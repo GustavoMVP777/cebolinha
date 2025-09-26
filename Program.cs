d151b12a732d7025ca10010e6d05f8f94a9be86c
gustavo <gustavo.sabio446688@gmail.com>
2025-09-26 19:47:31 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿﻿string fraseDigitada, Ceboles;
+
+Console.Write("Digite uma flase: ");
+fraseDigitada = Console.ReadLine()!;
+
+Ceboles = fraseDigitada
+    .Replace("r", "l")
+    .Replace("R", "L");
+
+Console.WriteLine(Ceboles);
